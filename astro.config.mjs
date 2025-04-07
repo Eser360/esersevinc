246cfd6af5b89ffc866cded92fd7acf346fd81a9
eser360 <eser360@gmail.com>
2025-04-07 07:50:39 +0000

    v1.79.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,9 +35,9 @@ export default defineConfig({
     tailwind({
       configFile: "./tailwind.config.mjs",
     }),
-    partytown({ config: { forward: ['dataLayer.push'] } }),
     playformCompress(),
   ],
+  integrations: [partytown({ config: { forward: ['dataLayer.push'] } })],
   markdown: {
     shikiConfig: {
       theme: CODE_THEME,
